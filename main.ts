2c0fd2fbf20305cb0d480c96b549ba567d0cd840
Trenton200913 <111209161+Trenton200913@users.noreply.github.com>
2023-10-09 13:06:30 -0500

    You can lose in the molble port

--- main.ts
+++ main.ts
@@ -3,6 +3,10 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         mySprite.vy = -90
     }
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
+    game.setGameOverMessage(false, "falled")
+    game.gameOver(false)
+})
 let mySprite: Sprite = null
 scene.setBackgroundColor(8)
 mySprite = sprites.create(img`
